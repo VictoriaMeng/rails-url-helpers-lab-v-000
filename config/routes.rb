2f448506b64bcb9557464da78f81a0c53cd0777f
Victoria Meng <nights1ststar@protonmail.com>
2018-06-29 03:46:30 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # resources :students, only: [:index, :show]
   # However, for the sake of this lab and seeing how you can pass params through
   # the route, we'll keep it explicit for now and refactor later.
-  resources :students, only: [:index, :show, :activate]
+  resources :students, only: [:index, :show]
+  get 'student/:id/activate', to 'students#activate', as: 'activate_student'
 end

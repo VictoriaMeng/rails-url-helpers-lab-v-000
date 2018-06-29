6d170fbe729525afeafb539791a800ca586b5b28
Victoria Meng <nights1ststar@protonmail.com>
2018-06-29 03:58:06 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   def activate
     @student = Student.find(params[:id])
     @student.active ? @student.update(active: false) : @student.update(active: true)
-    redirect_to student(@student)
+    redirect_to action: "show", id: params[:id]
   end
 
   private
